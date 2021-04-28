1d4d87e7714898d752f2ffc4f2898fbed9889192
MRGRD56 <kirgol85@gmail.com>
2021-04-28 14:14:44 +0500

    some changes

---
+++ WsrApp/ViewModels/PagesViewModels/ConsultationsPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WsrApp.Model;
+
+namespace WsrApp.ViewModels.PagesViewModels
+{
+    public class ConsultationsPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<Consultation> UnacceptedConsultations { get; set; } = new();
+
+        public ConsultationsPageViewModel()
+        {
+            LoadConsultations();
+        }
+
+        private async void LoadConsultations()
+        {
+            var consultationsRaw = await App.HttpClient.GetStringAsync($"{App.ApiUrl}/api/consultations/unaccepted?token={Account.Token}");
+            var consultations = JsonConvert.DeserializeObject<List<Consultation>>(consultationsRaw);
+            UnacceptedConsultations.Clear();
+            consultations.ForEach(UnacceptedConsultations.Add);
+        }
+    }
+}
